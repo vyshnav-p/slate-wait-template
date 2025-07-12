3a94c752a74125e8f7b9797fa9a7a8f7f498f76c
vyshnav-p <vyshnav8090@gmail.com>
2025-07-12 14:31:36 +0530

    Update about section

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,59 +1,52 @@
-import { motion } from 'motion/react'
 import React from 'react'
-import Image from 'next/image'
 import { HiOutlineSparkles } from 'react-icons/hi'
 import { LuChartNoAxesColumn, LuZap } from 'react-icons/lu'
 
 export default function About() {
     return (
-        <section className="w-[50rem] min-h-screen mx-auto z-10 bg-background relative pt-32">
-            <div className="w-fit flex items-center justify-center gap-1.5 rounded-lg bg-surface border border-surface-border light:border-none shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-none p-2">
+        <section className="w-[50rem] min-h-screen flex flex-col items-center mx-auto z-10 bg-background relative pt-40">
+            <div className="w-fit self-start flex items-center justify-center gap-1.5 rounded-lg bg-surface border border-surface-border light:border-none shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-none p-2">
                 <p className="text-xs text-foreground-muted">About Toko</p>
             </div>
-            {/*  */}
-            <div className="w-full grid grid-rows-2 gap-5 mt-10">
 
-                {/* Row 1 */}
-                <div className="w-full flex items-center justify-between gap-5 min-h-20">
-                    <div className="h-full place-content-center bg-surface-muted rounded-2xl border border-surface-muted-border max-w-xl p-5">
-                        <h1 className="text-lg font-semibold text-foreground-muted">
-                            Toko is the fastest way to build a website. No templates. No drag and drop. No friction.
-                            Just tell Toko what you need, and it builds it for you. Instantly. Clean, responsive, and ready to go live.
-                        </h1>
-                    </div>
+            <div className='flex flex-col w-full items-center justify-center mt-5'>
 
-                    <div className='flex relative w-full h-full rounded-2xl'>
-                        <Image
-                            src={'/dashboard.png'}
-                            alt={'dashboard'}
-                            fill
-                            className="object-cover"
-                        />
+                <h1 className="font-semibold text-foreground-muted text-2xl">
+                    Toko is the <span className=''>fastest way to build a website</span>. No templates. No drag and drop. No friction.
+                    Just tell Toko what you need, and it builds it for you. Instantly. Clean, responsive, and ready to go live.
+                </h1>
 
-                    </div>
+                <div className="w-full relative flex items-stretch justify-between gap-4 h-[6rem] mt-5">
+                    {[
+                        { icon: <HiOutlineSparkles className="size-6" />, label: "Advanced ML & NLP" },
+                        { icon: <LuChartNoAxesColumn className="size-6" />, label: "Easy Analytics" },
+                        { icon: <LuZap className="size-6" />, label: "10x Faster" },
+                        { icon: <LuZap className="size-6" />, label: "10x Faster" },
+                    ].map((item, idx) => (
+                        <div
+                            key={idx}
+                            className="h-full w-full flex flex-col items-center justify-center gap-3 bg-surface-muted rounded-2xl border border-surface-muted-border"
+                        >
+                            {item.icon}
+                            <p className="font-semibold text-sm">{item.label}</p>
+                        </div>
+                    ))}
                 </div>
 
-                {/* Row 2 - Grid for equal width cards */}
-                <div className="w-full grid grid-cols-4 gap-5 h-full">
-                    <div className="flex flex-col items-center justify-center gap-3 bg-surface-muted rounded-2xl border border-surface-muted-border py-5">
-                        <HiOutlineSparkles className="text-icon-foreground size-6" />
-                        <p className="text-foreground-muted font-semibold">Advanced ML & NLP</p>
-                    </div>
+                {/*  */}
 
-                    <div className="flex flex-col items-center justify-center gap-3 bg-surface-muted rounded-2xl border border-surface-muted-border py-5">
-                        <LuChartNoAxesColumn className="text-icon-foreground size-6" />
-                        <p className="text-foreground-muted font-semibold">Easy Analytics</p>
-                    </div>
+                <div className='w-full h-[30rem] max-h-[30rem] mt-10'>
+                    <video
+                        src="/team.mp4"
+                        className="w-full h-full object-cover rounded-[2.3rem] border-[7px] border-surface-muted-border"
+                        loop
+                        autoPlay
+                        playsInline
+                        muted
+                    />
 
-                    <div className="flex flex-col items-center justify-center gap-3 bg-surface-muted rounded-2xl border border-surface-muted-border py-5">
-                        <LuZap className="text-icon-foreground size-6" />
-                        <p className="text-foreground-muted font-semibold">10x Faster</p>
-                    </div>
-                     <div className="flex flex-col items-center justify-center gap-3 bg-surface-muted rounded-2xl border border-surface-muted-border py-5">
-                        <LuZap className="text-icon-foreground size-6" />
-                        <p className="text-foreground-muted font-semibold">10x Faster</p>
-                    </div>
                 </div>
+
             </div>
 
         </section>

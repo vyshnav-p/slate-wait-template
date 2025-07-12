91eb38a7e469706ab05bc527d275fc3247cc5c99
vyshnav-p <vyshnav8090@gmail.com>
2025-07-12 23:21:52 +0530

    Add proper responsive design for smaller screens

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,21 +5,21 @@ import { CgWebsite } from 'react-icons/cg'
 
 export default function Hero() {
   return (
-     <section id="hero" className="w-[50rem] mx-auto flex flex-col items-center text-center pt-48">
+     <section id="hero" className="max-w-[90vw] md:w-[45rem] lg:w-[50rem]  mx-auto flex flex-col items-center text-center pt-48">
         <motion.div initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3 }} className="flex items-center justify-center gap-1.5 rounded-lg bg-surface border border-surface-border light:border-none shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-none p-2">
           <CgWebsite className="text-sm text-icon-foreground" />
           <p className="text-xs text-foreground-muted">AI Powered</p>
         </motion.div>
-        <motion.h1 initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.1 }} className="text-6xl font-semibold mt-3 max-w-[36rem]">Build Stunning Websites in Seconds</motion.h1>
-        <motion.p initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.3 }} className="max-w-[25rem] mt-3 text-[#959595]">Create pixel-perfect websites with the power of AI. Just drag, drop, and go live.</motion.p>
-        <motion.div initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.5 }} className="relative w-[25rem] h-[3rem] mt-8 ">
+        <motion.h1 initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.1 }} className="text-3xl md:text-6xl font-semibold mt-3 max-w-[36rem]">Build Stunning Websites in Seconds</motion.h1>
+        <motion.p initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.3 }} className="text-sm md:text-base max-w-[25rem] mt-3 text-[#959595]">Create pixel-perfect websites with the power of AI. Just drag, drop, and go live.</motion.p>
+        <motion.div initial={{ y: 10, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.3, delay: 0.5 }} className="relative w-full md:w-[25rem] h-[3rem] mt-8 ">
           <input
             type="text"
             placeholder="Your email"
-            className="w-full h-full px-3 pr-[7rem] bg-surface-muted focus:bg-surface transition-colors duration-150 outline-none focus:outline-none border border-surface-muted-border focus:border-surface-border rounded-xl"
+            className="w-full h-full text-sm px-3 pr-[7rem] bg-surface-muted focus:bg-surface transition-colors duration-150 outline-none focus:outline-none border border-surface-muted-border focus:border-surface-border rounded-xl"
           />
           <button
-            className="absolute text-foreground-inverse  top-1 bottom-1 right-1 px-4 bg-button-surface text-sm font-medium rounded-xl border border-surface-border"
+            className="absolute text-foreground-inverse top-1 bottom-1 right-1 px-4 bg-button-surface text-sm font-medium rounded-xl border border-surface-border"
           >
             Join waitlist
           </button>
@@ -47,21 +47,20 @@ export default function Hero() {
         {/* device */}
 
         <div className="relative z-0 w-full">
-          <motion.div initial={{ y: 200 }} animate={{ y: 0 }} transition={{ duration: 0.5,delay: 0.8 }} className="flex items-center justify-center w-full h-[32rem] bg-[#7a7b7c] rounded-3xl mt-24 p-0.5 z-0">
+          <motion.div initial={{ y: 200 }} animate={{ y: 0 }} transition={{ duration: 0.5,delay: 0.8 }} className="flex items-center justify-center w-full h-[12rem] md:h-[32rem] bg-[#7a7b7c] rounded-3xl mt-24 p-0.5 z-0">
             <div className="flex items-center justify-center w-full h-full rounded-3xl p-2 bg-black">
               <div className="relative flex items-center justify-center w-full h-full rounded-2xl overflow-hidden">
                 <Image
                   src="/dashboard.png"
                   alt="Dashboard"
                   fill
-                  className="object-cover"
+                  className="object-cover object-top-left"
                 />
               </div>
-
             </div>
           </motion.div>
           {/*  */}
-          <div className="absolute bottom-0 left-0 w-full h-36 bg-gradient-to-t from-background to-transparent" />
+          <div className="absolute bottom-0 left-0 w-full h-20 md:h-36 bg-gradient-to-t from-background to-transparent" />
         </div>
 
 

476a009791e4eff1d7e4ca34c0d99272f0a3405e
vyshnav-p <vyshnav8090@gmail.com>
2025-07-18 13:43:02 +0530

    Fix theme toggle switching issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,39 @@
-'use client'
+"use client";
 
-import React from 'react'
-import Image from 'next/image'
+import Image from "next/image";
 import { FiSun } from "react-icons/fi";
 import { FaRegMoon } from "react-icons/fa";
-import { useTheme } from 'next-themes';
-import { motion } from 'motion/react';
+import { useTheme } from "next-themes";
+import { motion } from "motion/react";
 
 export default function Navbar() {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light")
-  }
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
-    <motion.div initial={{opacity:0}} animate={{opacity:1}} className='fixed w-80 left-0 right-0 top-10 mx-auto flex p-2 pl-3 items-center justify-between gap-4 bg-surface border border-surface-border rounded-2xl shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-none z-50'>
-        <Image
-          src={theme === "light" ? "/logo_light.png" : "/logo_dark.png"}
-          alt="logo"
-          width={25}
-          height={25}
-          priority
-        />
-      <button onClick={toggleTheme} className="bg-toggle-bg rounded-xl w-10 h-10 grid place-items-center border border-toggle-border">
-        <FiSun className="h-[1rem] w-[1rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 text-icon-foreground" />
-        <FaRegMoon className="absolute h-[1rem] w-[1rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0 text-icon-foreground" />
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="bg-surface border-surface-border fixed top-10 right-0 left-0 z-50 mx-auto flex w-80 items-center justify-between gap-4 rounded-2xl border p-2 pl-3 shadow-[0_8px_30px_rgb(0,0,0,0.12)] dark:shadow-none"
+    >
+      <Image
+        src={theme === "light" ? "/logo_light.png" : "/logo_dark.png"}
+        alt="logo"
+        width={25}
+        height={25}
+        priority
+      />
+      <button
+        onClick={toggleTheme}
+        className="bg-toggle-bg border-toggle-border grid h-10 w-10 place-items-center rounded-xl border"
+      >
+        <FiSun className="text-icon-foreground h-[1rem] w-[1rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <FaRegMoon className="text-icon-foreground absolute h-[1rem] w-[1rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         <span className="sr-only">Toggle theme</span>
       </button>
     </motion.div>
-  )
+  );
 }
